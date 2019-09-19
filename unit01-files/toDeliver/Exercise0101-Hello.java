52b4591667c207a4965a1905eb003ce67ef66648
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-09-19 22:32:33 +0200

    Exercise 01.01 - "Hello" on the screen

---
+++ unit01-files/toDeliver/Exercise0101-Hello.java
@@ -0,0 +1,14 @@
+// Adrián Navarro Gabino
+
+/*
+ * Install the JDK, Eclipse and create a program that writes "Hello" on the
+ * screen.
+ */
+
+public class Exercise0101
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
