a6a616dd99bcec3f1451edd4f818e8755e43c0c3
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-09-29 10:37:56 +0200

    Exercise 1.5.2 - Deserialize people data

---
+++ unit01-files/exercises/Exercise152-DeserializePeople.java
@@ -0,0 +1,92 @@
+// Adrián Navarro Gabino
+
+/*
+ * 1.5.2: Make a program that shows the data of the three people who had been
+ * saved in the file "people.dat".
+ */
+
+import java.io.*;
+import java.lang.ClassNotFoundException;
+
+class Person implements Serializable
+{
+	private String name;
+	private String mail;
+	private String date;
+	
+	public Person()
+	{
+		
+	}
+	
+	public Person(String name, String mail, String date)
+	{
+		this.name = name;
+		this.mail = mail;
+		this.date = date;
+	}
+
+	public String getName() 
+	{
+		return name;
+	}
+
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+
+	public String getMail() 
+	{
+		return mail;
+	}
+
+	public void setMail(String mail) 
+	{
+		this.mail = mail;
+	}
+
+	public String getDate() 
+	{
+		return date;
+	}
+
+	public void setDate(String date) 
+	{
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", mail=" + mail + ", date=" +
+            date + "]";
+	}
+}
+
+public class Exercise152
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			File file = new File("people.dat");
+		    FileInputStream fileIn = new FileInputStream(file);
+		    ObjectInputStream objectFile = new ObjectInputStream(fileIn);
+		    Person p = (Person)objectFile.readObject();
+            System.out.println(p);
+            p = (Person)objectFile.readObject();
+		    System.out.println(p);
+            p = (Person)objectFile.readObject();
+		    System.out.println(p);
+		    objectFile.close();
+		}
+        catch(ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
