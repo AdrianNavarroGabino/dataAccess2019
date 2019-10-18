ab46048da45aa8be44106edde8dfd266f4ecd6bd
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-10-18 16:09:24 +0200

    Exercise02.06 - Table Providers

---
+++ unit02-databasesAccess/toDeliver/Exercise0206-TableProviders.java
@@ -0,0 +1,46 @@
+// Adrián Navarro Gabino
+
+/*
+ * From Java, it expands the database "dia02", adding a table "proveedores"
+ * (with structure similar to that of "clientes").
+ * 
+ * Check from psql or pgAdmin that it was created correctly.
+ * 
+ * What happens if you create it twice?
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Exercise0206 
+{
+	static String url = "jdbc:postgresql://localhost:5432/dia02";
+	static String user = "postgres";
+	static String password = "1234";
+	static String sql = "";
+	
+    public static void main( String[] args )
+    {
+        try
+		{
+        	sql = "CREATE TABLE proveedores" +
+        			"(CODIGO VARCHAR(4) PRIMARY KEY, NOMBRE VARCHAR(50));";
+        	
+			Class.forName("org.postgresql.Driver");
+			Connection conn = DriverManager.getConnection(url, user, password);
+			Statement statement = conn.createStatement();
+			statement.executeUpdate(sql);
+			System.out.println("Table created");
+			conn.close();
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+    }
+}
+
