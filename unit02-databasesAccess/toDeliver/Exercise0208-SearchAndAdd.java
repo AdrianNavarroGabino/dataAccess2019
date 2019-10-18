692b20a4d902359d81fa5862aa8899ae2b1c1324
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-10-18 16:10:20 +0200

    Exercise 02.08 - Search and add data with a menu

---
+++ unit02-databasesAccess/toDeliver/Exercise0208-SearchAndAdd.java
@@ -0,0 +1,126 @@
+// Adrián Navarro Gabino
+
+/*
+ * Create a Java program, which connects to the "dia01" database, and allows 
+ * both adding new articles and displaying those that contain a certain text 
+ * entered by the user.
+ * 
+ * You should show a menu with the options: 1-Search, 2-Add, 3-Exit
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Exercise0208
+{
+	static String url = "jdbc:postgresql://localhost:5432/dia01";
+	static String user = "postgres";
+	static String password = "1234";
+	static String sql = "";
+	static Scanner sc = new Scanner(System.in);
+	
+    public static void main( String[] args )
+    {
+    	int option;
+    	do
+    	{
+	        System.out.println("1. Search article");
+	        System.out.println("2. Add article");
+	        System.out.println("3. Exit");
+	        System.out.print("Choose an option: ");
+	        option = sc.nextInt();
+	        sc.nextLine();
+	        
+	        if(option == 1)
+	        {
+	        	searchArticle();
+	        }
+	        else if(option == 2)
+	        {
+	        	addArticle();
+	        }
+	        else if(option == 3)
+	        {
+	        	System.out.println("Bye");
+	        }
+	        else
+	        {
+	        	System.out.println("Wrong option");
+	        }
+	        System.out.println();
+    	} while(option != 3);
+        
+    }
+    
+    public static void searchArticle()
+    {
+    	System.out.print("Article to search: ");
+    	String search = sc.nextLine().toUpperCase();
+    	
+    	boolean results = false;
+    	sql = "SELECT * FROM ARTICULOS WHERE UPPER(NOMBRE) LIKE '%" + 
+    			search + "%';";
+    	try
+		{
+			Class.forName("org.postgresql.Driver");
+			Connection conn = DriverManager.getConnection(url, user, password);
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			while(rs.next())
+			{
+				results = true;
+				String name = rs.getString("NOMBRE");
+				double price = rs.getDouble("PRECIO");
+				System.out.println("Name: " + name + " - Price: " + price + 
+						" €");
+			}
+			rs.close();
+			conn.close();
+			
+			if(!results)
+			{
+				System.out.println("There is no data");
+			}
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+    }
+    
+    public static void addArticle()
+    {
+    	System.out.print("Enter name: ");
+    	String name = sc.nextLine();
+    	System.out.print("Enter price: ");
+    	double price = Double.parseDouble(sc.nextLine().replace(',', '.'));
+    	
+    	sql = "INSERT INTO ARTICULOS (NOMBRE, PRECIO) VALUES ('" + name +
+    			"', '" + price + "');";
+    	
+    	try
+		{
+			Class.forName("org.postgresql.Driver");
+			Connection conn = DriverManager.getConnection(url, user, password);
+			Statement statement = conn.createStatement();
+			int affectedRows = statement.executeUpdate(sql);
+			if(affectedRows == 1)
+			{
+				System.out.println("Article inserted");
+			}
+			conn.close();
+		} catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+    }
+}
