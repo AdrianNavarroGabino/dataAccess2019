d009d5f8870afe623736ba59f0e822cc2501dd08
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-10-25 16:21:57 +0200

    Exercise 02.06 - Table Providers

--- unit02-databasesAccess/toDeliver/Exercise0206-TableProviders.java
+++ unit02-databasesAccess/toDeliver/Exercise0206-TableProviders.java
@@ -9,10 +9,7 @@
  * What happens if you create it twice?
  */
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Exercise0206 
 {
