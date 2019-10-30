4fd6278f3ebdf8fda9e5920af21b8111b7a7cad2
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-10-30 11:17:31 +0100

    Exercise 2.6.1 - Function FN_GET_DEVICE_NAME

---
+++ unit02-databasesAccess/exercises/Exercise261-GetNameFunction.java
@@ -0,0 +1,66 @@
+// Adrián Navarro Gabino
+
+/*
+ * 2.6.1: Creates a function in PL / pgSQL, which given the code of a copy 
+ * device of Security, return your name.
+ */
+
+/*
+CREATE OR REPLACE FUNCTION FN_GET_DEVICE_NAME(DEV_ID VARCHAR)
+RETURNS VARCHAR
+AS
+$$
+BEGIN
+	RETURN (SELECT NOMBRE
+		   FROM PRUEBA1
+		   WHERE CODIGO = DEV_ID);
+END
+$$
+LANGUAGE PLPGSQL;
+ */
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Scanner;
+
+import org.apache.commons.dbutils.DbUtils;
+
+public class Exercise261 {
+	public static void main(String[] args)
+	{
+		String url = "jdbc:postgresql://localhost:5432/pruebas1";
+		String user = "postgres";
+		String password = "1234";
+		
+		Connection conn = null;
+		CallableStatement cs = null;
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			conn = DriverManager.getConnection(url, user, password);
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter device id: ");
+			String deviceId = sc.nextLine();
+			
+			cs = conn.prepareCall("{call FN_GET_DEVICE_NAME(?)}");
+			cs.registerOutParameter(1, Types.VARCHAR);
+			cs.setString(1, deviceId);
+			cs.execute();
+			System.out.println("Name: " + cs.getString(1));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally
+		{
+			DbUtils.closeQuietly(cs);
+			DbUtils.closeQuietly(conn);
+		}
+	}
+}
