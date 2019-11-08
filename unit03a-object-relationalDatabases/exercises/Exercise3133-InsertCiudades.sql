e9de451dab57400320e665829dc1d795c8cc6e3f
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-11-08 17:55:46 +0100

    Exercise 3.1.3.3 - Insert Ciudades

--- unit03a-object-relationalDatabases/exercises/Exercise3133-InsertCiudades.sql
+++ unit03a-object-relationalDatabases/exercises/Exercise3133-InsertCiudades.sql
@@ -1,3 +1,12 @@
+-- Adrián Navarro Gabino
+
+/*
+ * 3.1.3.3: Add two example cities. On Wikipedia you can see the location of
+ * many cities, but usually in the format "second minute degree"; to see them
+ * as a single figure with decimals, it is usually enough to click on that
+ * location.
+ */
+
 INSERT INTO CIUDADES
 (NOMBRE, UBIC)
 VALUES
