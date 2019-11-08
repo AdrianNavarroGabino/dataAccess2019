7d380fc10b4e5717f65793f1094405075b1f2f41
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-11-08 17:56:04 +0100

    Exercise 3.1.4.1 - Enum

--- unit03a-object-relationalDatabases/exercises/Exercise3141-Enum.sql
+++ unit03a-object-relationalDatabases/exercises/Exercise3141-Enum.sql
@@ -1,2 +1,10 @@
+-- Adrián Navarro Gabino
+
+/*
+ * 3.1.4.1: Creates an enumerated data type called "Almac unit", which will
+ * refer to one of the most common storage units in computing: byte, kilobyte,
+ * megagbyte, gigabyte, terabyte.
+ */
+
 CREATE TYPE UNIDAD_ALMAC AS
 ENUM('byte', 'kilobyte', 'megabyte', 'gigabyte', 'terabyte');
