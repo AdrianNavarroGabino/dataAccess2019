f4336430ecdcede6856ab13baba6fa401b0afc0c
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-11-08 17:55:10 +0100

    Exercise 3.1.3.1 - Create type

--- unit03a-object-relationalDatabases/exercises/Exercise3131-DataType.sql
+++ unit03a-object-relationalDatabases/exercises/Exercise3131-DataType.sql
@@ -1,1 +1,8 @@
+-- Adrián Navarro Gabino
+
+/*
+ * 3.1.3.1: Creates a type of composite data called "location", which in a
+ * first approximation will consist of two real numbers: latitude and longitude.
+ */
+
 CREATE TYPE UBICACION AS (LATITUD FLOAT, LONGITUD FLOAT);
