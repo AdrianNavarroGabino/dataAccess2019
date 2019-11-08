1dba1d37e764416d3b3832e0010644741a891bad
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-11-08 17:54:51 +0100

    Exercise 3.1.2.2 - Domain cantidad_habitantes

--- unit03a-object-relationalDatabases/exercises/Exercise3122-Domain.sql
+++ unit03a-object-relationalDatabases/exercises/Exercise3122-Domain.sql
@@ -1,2 +1,9 @@
+-- Adrián Navarro Gabino
+
+/*
+ * 3.1.2.2: Create a domain called "numero_habitantes", which allows to
+ * save non-negative integers, of a maximum of 10 digits.
+ */
+
 CREATE DOMAIN cantidadHabitantes AS NUMERIC(10)
 CHECK (VALUE >= 0);
