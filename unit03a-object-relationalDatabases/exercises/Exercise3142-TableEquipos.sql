6af9bd56611fd98e335be9b68ed1d311c87745f2
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-11-08 17:56:20 +0100

    Exercise 3.1.4.2 - Create table Equipos

--- unit03a-object-relationalDatabases/exercises/Exercise3142-TableEquipos.sql
+++ unit03a-object-relationalDatabases/exercises/Exercise3142-TableEquipos.sql
@@ -1,3 +1,12 @@
+-- Adrián Navarro Gabino
+
+/*
+ * 3.1.4.2: Creates a "computers" database, with a first "computers" table.
+ * For each device we will store for the moment: a code ("cod"), the brand
+ * ("brand"), the model ("model"), the amount of memory ("cantMem", numerical)
+ * and the unit in which it is measured that memory ("unitMem").
+ */
+
 CREATE TABLE EQUIPOS
 (
 	COD VARCHAR(10) PRIMARY KEY,
