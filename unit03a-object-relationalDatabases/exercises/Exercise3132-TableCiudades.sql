8d6d48bc6e30ec04fedb2dc09caf3c0f79b64b0a
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-11-08 17:55:28 +0100

    Exercise 3.1.3.2 - Create table Ciudades

--- unit03a-object-relationalDatabases/exercises/Exercise3132-TableCiudades.sql
+++ unit03a-object-relationalDatabases/exercises/Exercise3132-TableCiudades.sql
@@ -1,3 +1,11 @@
+-- Adrián Navarro Gabino
+
+/*
+ * 3.1.3.2: In the "world" database, also create a "cities" table. For each
+ * country, for now we will save only three fields: a code ("cod"), the name
+ * ("name") and the location ("ubic").
+ */
+
 CREATE TABLE CIUDADES
 (
 	COD SERIAL PRIMARY KEY,
