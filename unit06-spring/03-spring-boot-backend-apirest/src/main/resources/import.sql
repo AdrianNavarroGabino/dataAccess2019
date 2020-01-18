a04539e82297f45d92658c292b6a6f4ab60d4bb8
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2020-01-18 17:44:26 +0100

    Spring. Building an API

---
+++ unit06-spring/03-spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Adrián', 'Navarro', 'adrian@adrian.com', '2020-01-18');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mr. John', 'Doe', 'john.doe@gmail.com', '2020-01-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Linus', 'Torvalds', 'linus@torvalds.com', '2020-01-13');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Rasmus', 'Lerdorf', 'rasmus@rasmus.com', '2020-01-05');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Erich', 'Gamma', 'eric@gamma.com', '2020-01-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Richard', 'Helm', 'richard@richard.com', '2020-01-17');
